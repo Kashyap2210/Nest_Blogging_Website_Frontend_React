af30220ab79bb9e217c8ce6ed41a9032831bd463
Kashyap Solanki <kash.cdac@gmail.com>
2025-01-29 20:53:43 +0530

    new routes add

--- src/components/BlogGrandContainer.tsx
+++ src/components/BlogGrandContainer.tsx
@@ -4,15 +4,17 @@ import BlogUpdate from './BlogUpdate';
 import HomePage from './Homepage';
 import Login from './Login';
 import UserCreate from './UserCreate';
+import Comment from './Comment';
 
 export default function BlogGrandContainer() {
   return (
     <Routes>
-        <Route path="/api" element={<HomePage/>} />
-        <Route path="/api/createBlog" element={<BlogCreate/>} />
-        <Route path="/api/updateBlog" element={<BlogUpdate/>} />
-        <Route path="/api/createUser" element={<UserCreate/>} />
-        <Route path="/api/login" element={<Login/>} />
+      <Route path="/api" element={<HomePage />} />
+      <Route path="/api/createBlog" element={<BlogCreate />} />
+      <Route path="/api/updateBlog" element={<BlogUpdate />} />
+      <Route path="/api/createUser" element={<UserCreate />} />
+      <Route path="/api/login" element={<Login />} />
+      <Route path="/api/createComment" element={<Comment />} />
     </Routes>
-    );
+  );
 }
