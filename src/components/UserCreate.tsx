0809d02801a5c344d94e8adc60f3616f9ce32739
Kashyap Solanki <kash.cdac@gmail.com>
2025-02-18 20:21:35 +0530

    Basic styling add to blogging form

--- src/components/UserCreate.tsx
+++ src/components/UserCreate.tsx
@@ -77,118 +77,86 @@ export default function UserCreate() {
   };
 
   return (
-    <div>
-      This is the form to create new User
-      <hr></hr>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={userData.name}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <br />
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={userData.username}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <br />
-        <input
-          type="text"
-          name="password"
-          placeholder="Password"
-          value={userData.password}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <br />
-        <input
-          type="text"
-          name="emailId"
-          placeholder="EmailId"
-          value={userData.emailId}
-          onChange={handleChange}
-        />
-        <br />
-        <br />
-        <br />
-        <input
-          type="text"
-          name="contactNo"
-          placeholder="Contact Number"
-          value={userData.contactNo}
-          onChange={handleChange}
-        />
-        {/* <br />
-        <br />
-        <br />
-        <input
-          type="text"
-          name="profilePictureUrl"
-          placeholder="Profile Picture"
-          value={userData.profilePictureUrl}
-          onChange={handleChange}
-        /> */}
-        <br />
-        <br />
-        <br />
-        <Button
-          component="label"
-          role={undefined}
-          variant="contained"
-          tabIndex={-1}
-          startIcon={<CloudUploadOutlined />}
+    <div className="flex flex-col items-center justify-center min-h-screen text-left p-4">
+      <h1 className="text-4xl font-bold text-red-500 mb-4">Create New User!</h1>
+      <div className="p-4">
+        <form
+          onSubmit={handleSubmit}
+          className="border p-8 mb-8 flex flex-col gap-8 items-center justify-center"
         >
-          Upload files
-          <VisuallyHiddenInput
-            type="file"
-            // name="file"
-            // placeholder="Profile Picture File"
-            onChange={handleFileUpload}
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={userData.name}
+            onChange={handleChange}
+            className="border px-4"
           />
-        </Button>
-        {/* <input
-          type="file"
-          name="file"
-          placeholder="Profile Picture File"
-          onChange={handleFileUpload}
-        /> */}
-        <br />
-        <br />
-        <br />
-        <select
-          name="gender"
-          value={userData.gender}
-          onChange={(e) =>
-            setUserData({ ...userData, gender: e.target.value as UserGender })
-          }
-        >
-          <option value={UserGender.MALE}>Male</option>
-          <option value={UserGender.FEMALE}>Female</option>
-          <option value={UserGender.PREFER_NOT_TO_SAY}>
-            Prefer Not to Say
-          </option>
-        </select>
-        <br />
-        <br />
-        <ColorButton type="submit">Create User</ColorButton>
-      </form>
-      <br />
-      <br />
-      <ColorButton>
-        <Link style={{ textDecoration: "none", color: "white" }} to="/api">
-          Go To HomePage
-        </Link>
-      </ColorButton>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={userData.username}
+            onChange={handleChange}
+            className="border px-4"
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="Password"
+            value={userData.password}
+            onChange={handleChange}
+            className="border px-4"
+          />
+          <input
+            type="text"
+            name="emailId"
+            placeholder="EmailId"
+            value={userData.emailId}
+            onChange={handleChange}
+            className="border px-4"
+          />
+          <input
+            type="text"
+            name="contactNo"
+            placeholder="Contact Number"
+            value={userData.contactNo}
+            onChange={handleChange}
+            className="border px-4"
+          />
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadOutlined />}
+            className="w-full"
+          >
+            Upload files
+            <VisuallyHiddenInput type="file" onChange={handleFileUpload} />
+          </Button>
+          <select
+            name="gender"
+            value={userData.gender}
+            onChange={(e) =>
+              setUserData({ ...userData, gender: e.target.value as UserGender })
+            }
+            className="w-full border"
+          >
+            <option value={UserGender.MALE}>Male</option>
+            <option value={UserGender.FEMALE}>Female</option>
+            <option value={UserGender.PREFER_NOT_TO_SAY}>
+              Prefer Not to Say
+            </option>
+          </select>
+          <ColorButton type="submit">Create User</ColorButton>
+        </form>
+        <ColorButton className="w-full">
+          <Link style={{ textDecoration: "none", color: "white" }} to="/api">
+            Go To HomePage
+          </Link>
+        </ColorButton>
+      </div>
       {newUser && (
         <div>
           <h2>Welcome {newUser.name}</h2>
