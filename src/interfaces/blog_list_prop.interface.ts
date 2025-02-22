43cd9ffef2e1c7e3ccc9f13b1f3435c11be92909
Kashyap Solanki <kash.cdac@gmail.com>
2025-02-23 00:30:01 +0530

    users array add to interface so that it is now passed as prop

--- src/interfaces/blog_list_prop.interface.ts
+++ src/interfaces/blog_list_prop.interface.ts
@@ -2,10 +2,12 @@ import {
   IBlogEntity,
   IBlogLikesCounterEntity,
   ICommentEntity,
+  IUserEntity,
 } from "blog-common-1.0";
 
 export interface IBlogListProps {
   likes: IBlogLikesCounterEntity[];
   blog: IBlogEntity;
   comments: ICommentEntity[];
+  users: IUserEntity[];
 }
