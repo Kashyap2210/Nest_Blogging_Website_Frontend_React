d19d355c152a9d08f2f92ebca80b47ee5619135c
Kashyap Solanki <kash.cdac@gmail.com>
2025-05-07 00:19:29 +0530

    searchUserByFilterApiCallFunction implement

--- src/api functions/users/users.api.calls.functions.ts
+++ src/api functions/users/users.api.calls.functions.ts
@@ -1,9 +1,12 @@
 import { getJwt } from "@/helpers/helper";
 import axios, { AxiosResponse } from "axios";
-import { IUserEntity, IUserProfileResponse } from "blog-common-1.0";
+import {
+  IUserEntity,
+  IUserEntityFilterData,
+  IUserProfileResponse,
+} from "blog-common-1.0";
 
 export const createUserApiCallFunction = async (
-  // e: React.FormEvent<HTMLFormElement>,
   formData: FormData
 ) => {
   const response: AxiosResponse<IUserEntity> = await axios.post(
@@ -19,7 +22,6 @@ export const createUserApiCallFunction = async (
 };
 
 export const getUserProfileApiCallFunction = async (
-  // e: React.FormEvent<HTMLFormElement>,
   id: number
 ) => {
   const response: AxiosResponse<IUserProfileResponse> = await axios.get(
@@ -33,16 +35,17 @@ export const getUserProfileApiCallFunction = async (
   return response?.data;
 };
 
-export const searchUserByFilterApiCallFunction = async () =>
-  {
-    const response: AxiosResponse<Partial<IUserEntity>[]> = await axios.post(
-      `http://localhost:3000/api/users/search`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${getJwt()}`, // Add token to header
-        },
-      }
-    );
-    return response?.data;
-  };
+export const searchUserByFilterApiCallFunction = async (
+  filter: IUserEntityFilterData
+): Promise<IUserEntity[]> => {
+  const response: AxiosResponse<IUserEntity[]> = await axios.post(
+    `http://localhost:3000/api/users/search`,
+    filter,
+    {
+      headers: {
+        Authorization: `Bearer ${getJwt()}`,
+      },
+    }
+  );
+  return response?.data;
+};
